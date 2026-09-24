2e4a7130fa7df08200c49c46a4c8d02b66399738
shinshin <rg26n055@guh.u-hyogo.ac.jp>
2026-09-24 13:40:18 +0900

    変更３回目データベース

--- firebase.js
+++ firebase.js
@@ -6,6 +6,15 @@ import {
   addDoc,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_5Nag4DsiRZedzMF5LL6sOCVyMgTaWbU",
+  authDomain: "smart-schedule-coordinat-79ae1.firebaseapp.com",
+  projectId: "smart-schedule-coordinat-79ae1",
+  storageBucket: "smart-schedule-coordinat-79ae1.firebasestorage.app",
+  messagingSenderId: "346372869754",
+  appId: "1:346372869754:web:bdef02ad29dafb2e15f5c3"
+};
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
@@ -20,7 +29,6 @@ async function testWrite() {
     });
 
     console.log("保存成功:", docRef.id);
-
   } catch (error) {
     console.error("保存エラー:", error);
   }
